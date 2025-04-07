e60f1386772c7597c299e8896a8f5ce7d3b15625
Miloni <matal@nvidia.com>
2025-04-07 23:49:30 +0000

    No impact from other types

--- cpp/include/cudf/ast/detail/expression_evaluator.cuh
+++ cpp/include/cudf/ast/detail/expression_evaluator.cuh
@@ -40,11 +40,8 @@ namespace detail {
 // TODO: duplicate
 CUDF_HOST_DEVICE constexpr bool is_complex_type(cudf::type_id type)
 {
-  // TODO: only decimals? impact of dictionary types, strings?
-  return type == cudf::type_id::STRUCT || type == cudf::type_id::LIST ||
-         type == cudf::type_id::DECIMAL32 || type == cudf::type_id::DECIMAL64 ||
-         type == cudf::type_id::DECIMAL128 || type == cudf::type_id::STRING ||
-         type == cudf::type_id::DICTIONARY32;
+  return type == cudf::type_id::DECIMAL32 || type == cudf::type_id::DECIMAL64 ||
+         type == cudf::type_id::DECIMAL128;
 }
 
 /**
--- cpp/src/transform/compute_column.cu
+++ cpp/src/transform/compute_column.cu
@@ -41,11 +41,8 @@ namespace detail {
 
 CUDF_HOST_DEVICE constexpr bool is_complex_type(cudf::type_id type)
 {
-  // TODO: only decimals? impact of dictionary types, strings?
-  return type == cudf::type_id::STRUCT || type == cudf::type_id::LIST ||
-         type == cudf::type_id::DECIMAL32 || type == cudf::type_id::DECIMAL64 ||
-         type == cudf::type_id::DECIMAL128 || type == cudf::type_id::STRING ||
-         type == cudf::type_id::DICTIONARY32;
+  return type == cudf::type_id::DECIMAL32 || type == cudf::type_id::DECIMAL64 ||
+         type == cudf::type_id::DECIMAL128;
 }
 
 /**
